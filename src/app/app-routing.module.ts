1a6625220cb6cc0a55465feb0a04184f1f4b7846
andmohiko <thomasito.berryz@gmail.com>
2020-02-29 12:54:24 +0900

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { WorksComponent } from './works/works.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+    },
+    {
+    path:  '',
+    component:  HomeComponent
+    },
+    {
+    path:  'about',
+    component:  AboutComponent
+    },
+    {
+    path:  'works',
+    component:  WorksComponent
+    },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
