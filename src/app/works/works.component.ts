e2f95e69a952fe123b4d029637fb42f3bc7ab186
andmohiko <thomasito.berryz@gmail.com>
2020-03-07 16:20:25 +0900

    heroku

--- src/app/works/works.component.ts
+++ src/app/works/works.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-works',
   templateUrl: './works.component.html',
-  styleUrls: ['./works.component.styl']
+  styleUrls: ['./works.component.styl'],
 })
 export class WorksComponent implements OnInit {
 
